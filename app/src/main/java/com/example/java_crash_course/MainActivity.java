f29459a47b54dfb681a8259d2ef488d230953e69
Nizaam Yunus <nizaamcoleyunus18@gmail.com>
2026-09-12 22:18:15 +0200

    Created a new screen and linked main and the second screen together

--- app/src/main/java/com/example/java_crash_course/MainActivity.java
+++ app/src/main/java/com/example/java_crash_course/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.java_crash_course;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,14 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("failing", "it's not working");
-        findViewById(R.id.button);
     }
 
-    public void handleText(View v) {
-        v.setEnabled(false);
-        Log.d("clicked", "Click method");
-        Toast.makeText(this, "clicked", Toast.LENGTH_LONG).show();
+    public void launchSettings(View view) {
+        //        launch a new activity
+        Intent i = new Intent(this, SettingsActivity.class);
+        startActivity(i);
     }
-
 }
